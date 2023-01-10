c1d15ea521b2ea64ce88f4bbab257849ad2747b1
ليزبيث هالا <llulah@live.com>
2023-01-09 21:06:43 -0300

    add results

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -12,6 +12,8 @@ func addDoubles(x: Double, y: Double) -> Double {
 
 let doubleSum = addDoubles(x: 1.0, y: 2.0)
 
+// MARK: Arrays
+
 let numbers = [1, 2, 3]
 
 let firstNumber = numbers[0]
@@ -23,8 +25,32 @@ numbersAgain.append(3)
 
 let firstNumberAgain = numbersAgain[0]
 
+// MARK: Dictionaries
+
 let countryCodes = ["Arendelle": "AR", "Genovia": "GN", "Freedonia": "FD"]
 let countryCode = countryCodes["Freedonia"]
 
+// MARK: Optionals
+
 let optionalName = Optional<String>.some("Princess Moana")
 if let name = optionalName {}
+
+// MARK: Results
+
+enum MagicError: Error {
+  case spellFailure
+}
+
+func cast(_ spell: String) -> Result<String, MagicError> {
+    switch spell {
+        case "flowers":
+            return .success("💐")
+        case "stars":
+            return .success("✨")
+        default:
+            return .failure(.spellFailure)
+    }
+}
+
+let result1 = cast("flowers") // .success("💐")
+let result2 = cast("avada kedavra") // .failure(.spellFailure)
