ead6463ea4e4b3df2364a5ecaec12b9f59a51b50
ليزبيث هالا <llulah@live.com>
2023-01-13 18:16:00 -0300

    add generic type and function

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -54,3 +54,42 @@ func cast(_ spell: String) -> Result<String, MagicError> {
 
 let result1 = cast("flowers") // .success("💐")
 let result2 = cast("avada kedavra") // .failure(.spellFailure)
+
+// MARK: Generic Data Structures
+
+struct Queue<Element> {
+    private var elements: [Element] = []
+    
+    mutating func enqueue(newElement: Element) {
+        elements.append(newElement)
+    }
+
+    mutating func dequeue() -> Element? {
+        guard !elements.isEmpty else { return nil }
+        return elements.remove(at: 0)
+    }
+}
+
+var q = Queue<Int>()
+
+q.enqueue(newElement: 4)
+q.enqueue(newElement: 2)
+
+q.dequeue()
+q.dequeue()
+q.dequeue()
+q.dequeue()
+
+// MARK: Generic Functions
+
+func pairs<Key, Value>(from dictionary: [Key: Value]) -> [(Key, Value)] {
+  return Array(dictionary)
+}
+
+let somePairs = pairs(from: ["minimum": 199, "maximum": 299])
+// result is [("maximum", 299), ("minimum", 199)]
+
+let morePairs = pairs(from: [1: "Swift", 2: "Generics", 3: "Rule"])
+// result is [(1, "Swift"), (2, "Generics"), (3, "Rule")]
+
+// MARK: Generic Functions
